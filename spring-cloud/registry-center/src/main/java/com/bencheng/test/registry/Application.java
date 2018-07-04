4342abb3f199282dcdc2de63da517f5d346dd78f
BenchengZhuo <270293025@qq.com>
2018-07-04 15:26:57 +0800

    Add dependency for eureka

--- spring-cloud/registry-center/src/main/java/com/bencheng/test/registry/Application.java
+++ spring-cloud/registry-center/src/main/java/com/bencheng/test/registry/Application.java
@@ -1,9 +1,16 @@
 package com.bencheng.test.registry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * Created by Bencheng on 2018/6/28.
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class);
     }
 }
